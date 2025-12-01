ab1d45b21e9bed52ef315b27b236c151bddea899
rahulxyzten <beherarahul101@gmail.com>
2025-12-02 01:04:37 +0530

    Initial Commit (Project Setup)
    Set up a new Next.js app with custom fonts, global styles, and initial components including Header and Navbar. Added configuration files, constants, type definitions, and sample assets for icons, images, and videos.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+const Page = () => {
+  return (
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Library"/>
+      <h1 className="text-2xl font-karla">Welcome to Loom Clone</h1>
+    </main>
+  );
+};
+
+export default Page;
